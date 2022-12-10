879ea07b66128fed6232e5033bd3476f408309f7
rodionzam <rodionzam@yandex.ru>
2022-12-10 21:50:09 +0300

    Part 4 / Lesson 72

--- Part-4/Проект FOOD/src/js/main.js
+++ Part-4/Проект FOOD/src/js/main.js
@@ -29,7 +29,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
         if (target && target.classList.contains('tabheader__item')) {
             tabs.forEach((item, i) => {
-                if(target == item) {
+                if(target === item) {
                     hideTabContent();
                     showTabContent(i);
                 }
@@ -100,4 +100,40 @@ window.addEventListener('DOMContentLoaded', () => {
 
     setClock('.timer', deadline);
     //#endregion Timer
+
+    //#region Modal
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close]');
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            // modal.classList.toggle('show');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        // modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+    //#endregion Modal
 });
