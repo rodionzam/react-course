53e60ac5f8ae598cb5eaa1f74e4c904e5c65d212
rodionzam <rodionzam@yandex.ru>
2022-12-17 22:31:22 +0300

    Part 5 / Lesson 87

--- Part-4/Проект FOOD/gulpfile.js
+++ Part-4/Проект FOOD/gulpfile.js
@@ -10,53 +10,53 @@ const dist = "./dist";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
-                .pipe(gulp.dest(dist))
-                .pipe(browsersync.stream());
+        .pipe(gulp.dest(dist))
+        .pipe(browsersync.stream());
 });
 
 gulp.task("copy-php", () => {
     return gulp.src("./src/server.php")
-                .pipe(gulp.dest(dist))
-                .pipe(browsersync.stream());
+        .pipe(gulp.dest(dist))
+        .pipe(browsersync.stream());
 });
 
 gulp.task("build-js", () => {
     return gulp.src("./src/js/main.js")
-                .pipe(webpack({
-                    mode: 'development',
-                    output: {
-                        filename: 'script.js'
-                    },
-                    watch: false,
-                    devtool: "source-map",
-                    module: {
-                        rules: [
-                          {
-                            test: /\.m?js$/,
-                            exclude: /(node_modules|bower_components)/,
-                            use: {
-                              loader: 'babel-loader',
-                              options: {
+        .pipe(webpack({
+            mode: 'development',
+            output: {
+                filename: 'script.js'
+            },
+            watch: false,
+            devtool: "source-map",
+            module: {
+                rules: [
+                    {
+                        test: /\.m?js$/,
+                        exclude: /(node_modules|bower_components)/,
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
                                 presets: [['@babel/preset-env', {
                                     debug: true,
-                                    corejs: 3,
-                                    useBuiltIns: "usage"
+                                    corejs: 3.22,
+                                    useBuiltIns: "entry"
                                 }]]
-                              }
                             }
-                          }
-                        ]
-                      }
-                }))
-                .pipe(gulp.dest(dist + '/js'))
-                .pipe(browsersync.stream());
+                        }
+                    }
+                ]
+            }
+        }))
+        .pipe(gulp.dest(dist + '/js'))
+        .pipe(browsersync.stream());
 });
 
 gulp.task("build-sass", () => {
     return gulp.src("./src/scss/**/*.scss")
-                .pipe(sass().on('error', sass.logError))
-                .pipe(gulp.dest(dist + '/css'))
-                .pipe(browsersync.stream());
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(dist + '/css'))
+        .pipe(browsersync.stream());
 });
 
 gulp.task("copy-assets", () => {
@@ -64,15 +64,15 @@ gulp.task("copy-assets", () => {
         .pipe(gulp.dest(dist + "/icons"));
 
     return gulp.src("./src/img/**/*.*")
-                .pipe(gulp.dest(dist + "/img"))
-                .pipe(browsersync.stream());
+        .pipe(gulp.dest(dist + "/img"))
+        .pipe(browsersync.stream());
 });
 
 gulp.task("watch", () => {
     browsersync.init({
-		server: "./dist/",
-		port: 4000,
-		notify: true
+        server: "./dist/",
+        port: 4000,
+        notify: true
     });
 
     gulp.watch("./src/index.html", gulp.parallel("copy-html"));
@@ -101,25 +101,25 @@ gulp.task("prod", () => {
             },
             module: {
                 rules: [
-                  {
-                    test: /\.m?js$/,
-                    exclude: /(node_modules|bower_components)/,
-                    use: {
-                      loader: 'babel-loader',
-                      options: {
-                        presets: [['@babel/preset-env', {
-                            debug: false,
-                            corejs: 3,
-                            useBuiltIns: "usage"
-                        }]]
-                      }
+                    {
+                        test: /\.m?js$/,
+                        exclude: /(node_modules|bower_components)/,
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: [['@babel/preset-env', {
+                                    debug: false,
+                                    corejs: 3.22,
+                                    useBuiltIns: "entry"
+                                }]]
+                            }
+                        }
                     }
-                  }
                 ]
-              }
+            }
         }))
         .pipe(gulp.dest(dist + '/js'));
-    
+
     return gulp.src("./src/scss/style.scss")
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss([autoprefixer()]))
--- Part-4/Проект FOOD/src/js/main.js
+++ Part-4/Проект FOOD/src/js/main.js
@@ -245,34 +245,29 @@ window.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json');
-
             const formData = new FormData(form);
             const object = {};
-
             formData.forEach(function (value, key) {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    showThanksModal(message.success);
-                    form.reset();
-                    setTimeout(() => {
-                        statusMessage.remove();
-                    }, 2000);
-                } else {
-                    showThanksModal(message.failure);
-                    setTimeout(() => {
-                        statusMessage.remove();
-                    }, 2000);
-                }
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify(object),
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+                statusMessage.remove();
+            }).finally(() => {
+                form.reset();
             });
         });
     }
