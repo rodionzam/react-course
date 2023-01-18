8c3ae42f2da3bd19d64023f3f476cfadbe14c295
rodionzam <rodionzam@yandex.ru>
2023-01-19 00:33:24 +0300

    Part 6 / Lesson 123

--- Part-6/my-first-app/src/index.js
+++ Part-6/my-first-app/src/index.js
@@ -3,17 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const text = 'Hello World!';
-
-const elem = (
-    <div>
-        <h2>Текст: {text}</h2>
-        <input type="text"/>
-        <button>Click</button>
-    </div>
-);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
