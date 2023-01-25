49dd30e7bd73639cf352db839981a2342c14b4ad
rodionzam <rodionzam@yandex.ru>
2023-01-26 00:27:49 +0300

    Part 6 / Lesson 127

--- Part-6/employees/src/components/app/app.js
+++ Part-6/employees/src/components/app/app.js
@@ -6,6 +6,12 @@ import EmployeesList from "../employees-list/employees-list";
 import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
 function App() {
+    const data = [
+        {name: 'John C.', salary: 800, increase: false},
+        {name: 'Alex M.', salary: 3000, increase: true},
+        {name: 'Carl W.', salary: 5000, increase: false}
+    ];
+
     return (
         <div className="app">
             <AppInfo/>
@@ -15,7 +21,7 @@ function App() {
                 <AppFilter/>
             </div>
 
-            <EmployeesList/>
+            <EmployeesList data={data}/>
             <EmployeesAddForm/>
         </div>
     );
