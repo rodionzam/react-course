8b71c09eb6c50343988ec9aed9f3b31cf75a98d3
rodionzam <rodionzam@yandex.ru>
2023-05-02 21:25:04 +0300

    Part 7 / Lesson 159

--- Part-7/marvel/src/components/charList/CharList.js
+++ Part-7/marvel/src/components/charList/CharList.js
@@ -62,8 +62,20 @@ class CharList extends Component {
         })
     }
 
+    itemRefs = [];
+
+    setRef = (ref) => {
+        this.itemRefs.push(ref);
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+    }
+
     renderItems(arr) {
-        const items =  arr.map((item) => {
+        const items =  arr.map((item, i ) => {
             let imgStyle = {'objectFit' : 'cover'};
 
             if (item.thumbnail.includes('image_not_available')) {
@@ -73,8 +85,13 @@ class CharList extends Component {
             return (
                 <li
                     className="char__item"
+                    tabIndex={0}
+                    ref={this.setRef}
                     key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}>
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }}>
                     <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                     <div className="char__name">{item.name}</div>
                 </li>
